a7e817b39a467168c683cfa02296ff66d8324b4c
jsbednarski <128535436+jsbednarski@users.noreply.github.com>
2023-07-15 17:27:36 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Face Photo Restorer</title>
+        <title>Przywracanie Zdjęć Twarzy</title>
       </Head>
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
@@ -21,21 +21,19 @@ const Home: NextPage = () => {
           rel="noreferrer"
           className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out"
         >
-          Used by over <span className="font-semibold">300,000</span> happy
-          users
+          Używane przez ponad <span className="font-semibold">300 000</span> zadowolonych użytkowników
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
-          Restoring old photos{" "}
+          Przywracanie starych zdjęć{" "}
           <span className="relative whitespace-nowrap text-[#3290EE]">
             <SquigglyLines />
-            <span className="relative">using AI</span>
+            <span className="relative">z wykorzystaniem sztucznej inteligencji</span>
           </span>{" "}
-          for everyone.
+          dla każdego.
         </h1>
 
         <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">
-          Have old and blurry face photos? Let our AI restore them so those
-          memories can live on. 100% free – restore your photos today.
+          Posiadasz stare i rozmyte zdjęcia twarzy? Pozwól naszej sztucznej inteligencji je przywrócić, aby wspomnienia mogły trwać. 100% darmowe - przywróć swoje zdjęcia już dziś.
         </p>
         <div className="flex justify-center space-x-4">
           <a
@@ -44,23 +42,23 @@ const Home: NextPage = () => {
             target="_blank"
             rel="noreferrer"
           >
-            Learn how it's built
+            Dowiedz się, jak to działa
           </a>
 
           <Link
             className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
             href="/restore"
           >
-            Restore your photos
+            Przywróć swoje zdjęcia
           </Link>
         </div>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-2 sm:flex-row flex-col">
               <div>
-                <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
+                <h2 className="mb-1 font-medium text-lg">Oryginalne zdjęcie</h2>
                 <Image
-                  alt="Original photo of my bro"
+                  alt="Oryginalne zdjęcie mojego brata"
                   src="/michael.jpg"
                   className="w-96 h-96 rounded-2xl"
                   width={400}
@@ -68,9 +66,9 @@ const Home: NextPage = () => {
                 />
               </div>
               <div className="sm:mt-0 mt-8">
-                <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
+                <h2 className="mb-1 font-medium text-lg">Przywrócone zdjęcie</h2>
                 <Image
-                  alt="Restored photo of my bro"
+                  alt="Przywrócone zdjęcie mojego brata"
                   width={400}
                   height={400}
                   src="/michael-new.jpg"
