545c70aa61c2c5508539bdd89ee4cbaacc12347b
gbednarski <139589791+gbednarski@users.noreply.github.com>
2023-07-17 16:32:05 +0200

    Update appendNewToName.ts

--- utils/appendNewToName.ts
+++ utils/appendNewToName.ts
@@ -1,7 +1,7 @@
 export default function appendNewToName(name: string) {
-  let insertPos = name.indexOf(".");
-  let newName = name
-    .substring(0, insertPos)
-    .concat("-new", name.substring(insertPos));
-  return newName;
+  let insertPos = name.indexOf("."); // Znajduje pozycję pierwszego wystąpienia kropki w nazwie pliku
+  let newName = name // Tworzy nową nazwę pliku
+    .substring(0, insertPos) // Pobiera fragment nazwy przed kropką
+    .concat("-new", name.substring(insertPos)); // Łączy fragment przed kropką, "-new" oraz fragment po kropce
+  return newName; // Zwraca nową nazwę pliku
 }
