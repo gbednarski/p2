e524c51906657d6429e00272f1e6a39839e5d48f
gbednarski <139589791+gbednarski@users.noreply.github.com>
2023-07-17 16:31:38 +0200

    Update downloadPhoto.ts

--- utils/downloadPhoto.ts
+++ utils/downloadPhoto.ts
@@ -1,10 +1,10 @@
 function forceDownload(blobUrl: string, filename: string) {
-  let a: any = document.createElement("a");
-  a.download = filename;
-  a.href = blobUrl;
-  document.body.appendChild(a);
-  a.click();
-  a.remove();
+  let a: any = document.createElement("a"); // Tworzy element <a>
+  a.download = filename; // Ustawia atrybut download na nazwę pliku
+  a.href = blobUrl; // Ustawia atrybut href na blob URL
+  document.body.appendChild(a); // Dodaje element <a> do ciała dokumentu
+  a.click(); // Wywołuje zdarzenie kliknięcia na elemencie <a>
+  a.remove(); // Usuwa element <a> z dokumentu
 }
 
 export default function downloadPhoto(url: string, filename: string) {
@@ -14,10 +14,10 @@ export default function downloadPhoto(url: string, filename: string) {
     }),
     mode: "cors",
   })
-    .then((response) => response.blob())
+    .then((response) => response.blob()) // Pobiera dane jako obiekt Blob
     .then((blob) => {
-      let blobUrl = window.URL.createObjectURL(blob);
-      forceDownload(blobUrl, filename);
+      let blobUrl = window.URL.createObjectURL(blob); // Tworzy blob URL dla obiektu Blob
+      forceDownload(blobUrl, filename); // Wywołuje funkcję forceDownload() z blob URL i nazwą pliku
     })
-    .catch((e) => console.error(e));
+    .catch((e) => console.error(e)); // Wyświetla błąd w konsoli, jeśli wystąpił
 }
